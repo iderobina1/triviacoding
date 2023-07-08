8072f7644c2b4196f40bc34689a22ed23b93cd14
iderobina <iderobina@gmail.com>
2023-07-07 19:39:32 -0600

    adding readme file

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# triviacoding
+Description
+Coding multiple Choice trivia game. 
+
+Usage
+No special usage needed for this project. Accesible via any browser.
+
+
+Questions
+If you have any questions about the repo, open an issue or contact me directly at iderobina@gmail.com
+
+Achnolowdgement to EDX Bootcamp.
+
+last updated 07/07/2023
--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,8 @@
-// variables to keep track of quiz state
 var currentQuestion = 0;
 var time = 100;
 var score = 0;
 var timerId;
 
-// variables to reference DOM elements
 var questions = document.getElementById('questions');
 var timer = document.getElementById('time');
 var scoreValue = document.getElementById('scoreValue');
@@ -53,14 +51,11 @@ function startQuiz() {
   var startScreen = document.getElementById('screen');
   startScreen.style.display = 'none';
 
-  // show questions section
   var questionsSection = document.getElementById('questions');
   questionsSection.style.display = 'block';
 
-  // start timer
   timerId = setInterval(clockTick, 1000);
 
-  // show starting time
   timer.textContent = time;
 
   getQuestion();
@@ -69,14 +64,11 @@ function startQuiz() {
 function getQuestion() {
   var currentQuestionData = questionsData[currentQuestion];
 
-  // update title with current question
   var titleEl = document.getElementById('qtitle');
   titleEl.textContent = currentQuestionData.title;
 
-  // clear out any old question choices
   choices.innerHTML = '';
 
-  // loop over choices
   for (var i = 0; i < currentQuestionData.choices.length; i++) {
     var choice = currentQuestionData.choices[i];
     var choiceNode = document.createElement('button');
@@ -85,7 +77,6 @@ function getQuestion() {
 
     choiceNode.textContent = i + 1 + '. ' + choice;
 
-    // display on the page
     choices.appendChild(choiceNode);
   }
 }
@@ -93,43 +84,34 @@ function getQuestion() {
 function questionClick(event) {
   var buttonEl = event.target;
 
-  // if the clicked element is not a choice button, do nothing.
   if (!buttonEl.matches('.choice')) {
     return;
   }
 
-  // check if user guessed wrong
   if (buttonEl.value !== questionsData[currentQuestion].answer) {
-    // penalize time
     time -= 15;
 
     if (time < 0) {
       time = 0;
     }
 
-    // display new time on page
     timer.textContent = time;
 
-    // Provide feedback to the user
     var feedbackEl = document.getElementById('feedback');
     feedbackEl.textContent = 'Wrong!';
   } else {
     var feedbackEl = document.getElementById('feedback');
     feedbackEl.textContent = 'Correct!';
-    // increase score
     score += 10;
     scoreValue.textContent = score;
   }
 
-  // move to next question
   currentQuestion++;
 
-  // clear feedback message
   setTimeout(function () {
     feedbackEl.textContent = '';
   }, 1000);
 
-  // check if we've run out of questions
   if (currentQuestion === questionsData.length) {
     quizEnd();
   } else {
@@ -138,73 +120,55 @@ function questionClick(event) {
 }
 
 function quizEnd() {
-  // stop timer
   clearInterval(timerId);
 
-  // show end screen
   var endScreen = document.getElementById('theend');
   endScreen.style.display = 'block';
 
-  // show final score
   var finalScore = document.getElementById('finalscore');
   finalScore.textContent = score;
 
-  // hide questions section
   questions.style.display = 'none';
 }
 
 function clockTick() {
-  // update time
   time--;
   timer.textContent = time;
 
-  // check if user ran out of time
   if (time <= 0) {
     quizEnd();
   }
 }
 
 function saveHighscore(event) {
-  event.preventDefault(); // Prevent the default form submission behavior
-
-  // get value of input box
+  event.preventDefault(); 
   var initials = nameInput.value.trim();
 
-  // make sure value wasn't empty
   if (initials !== '') {
-    // get saved scores from localstorage, or if not any, set to empty array
     var highscores = JSON.parse(localStorage.getItem('highscores')) || [];
 
-    // format new score object for current user
     var newScore = {
       score: score,
       initials: initials,
     };
 
-    // save to localstorage
     highscores.push(newScore);
     localStorage.setItem('highscores', JSON.stringify(highscores));
 
-    // redirect to next page
     window.location.href = 'scores.html';
   }
 }
 
 submit.addEventListener('click', saveHighscore);
 
-// user clicks button to start quiz
 start.addEventListener('click', startQuiz);
 
-// Attach event listener to choices container
 choices.addEventListener('click', questionClick);
 
-// Retrieve the highscores from local storage
 var highscores = JSON.parse(localStorage.getItem('highscores')) || [];
 
-// Clear the scoreList
 scoreList.innerHTML = '';
 
-// Iterate over the highscores and create list items to display them
 for (var i = 0; i < highscores.length; i++) {
   var scoreData = highscores[i];
   var listItem = document.createElement('li');
